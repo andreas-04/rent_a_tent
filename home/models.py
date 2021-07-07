f48877422212ce662d608216aa406e3858e4f500
Andreas <andreasneacsu@gmail.com>
2021-07-07 18:41:57 -0500

    updated database listing model to include availability and publishing date

--- home/models.py
+++ home/models.py
@@ -13,7 +13,9 @@ class Listing(models.Model):
     )
     title = models.CharField(max_length=150)
     catagory = models.CharField(max_length=21, blank=True, null=True, choices = listing_description_choices)
+    available_till = models.DateField('Available till')
     description = models.TextField(max_length=2000)
+    pub_date = models.DateTimeField('Date published', auto_now = True)
 #   images = models.ImageField //ADD AN IMAGE FIELD FOR MODEL
     def __str__(self):
         return self.title
