efcbdea23b48b2cb6d53e76063775137930aecfc
Andreas <andreasneacsu@gmail.com>
2021-07-08 00:12:40 -0500

    updated listings views

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .forms import ListingForm
 # Create your views here.
+def listings(request):
+    return render(request, 'listings.html')
+
+def new(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+    else:
+        form = ListingForm('ModelForm')
+    return render(request, 'newlisting.html', {'form': form})
