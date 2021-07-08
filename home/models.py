fd1626151eb3ba4edb34b37deada9cbe7f4aab3b
Andreas <andreasneacsu@gmail.com>
2021-07-08 00:08:33 -0500

    fixed listing model

--- home/models.py
+++ home/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Listing(models.Model):
-    listing_description_choices = (
+listing_description_choices = (
         ("Camping Equipment", "Camping Equipment"),
         ("kayaks and Canoes", "kayaks and Canoes"),
         ('Fishing Equipment', "Fishing Equipment"),
@@ -11,6 +10,8 @@ class Listing(models.Model):
         ('Other', "Other"),
         (None, 'Choose a Catagory')
     )
+
+class Listing(models.Model):
     title = models.CharField(max_length=150)
     catagory = models.CharField(max_length=21, blank=True, null=True, choices = listing_description_choices)
     available_till = models.DateField('Available till')
