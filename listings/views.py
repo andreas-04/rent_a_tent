4c47a10dc3fd9fd76b601e1febe48c721499b923
Andreas <andreasneacsu@gmail.com>
2021-07-12 14:48:48 -0500

    requires user to be logged in to post listings

--- listings/views.py
+++ listings/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import redirect, render
 from . forms import ListingForm
+from django.conf import settings
 # Create your views here.
 def listings(request):
     return render(request, 'listings.html')
 
 def new(request):
-    if request.method == 'POST':
-        form = ListingForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('homepage')
-
+    if not request.user.is_authenticated:
+        return redirect('login')
     else:
-        form = ListingForm()
-    return render(request, 'newlisting.html', {'form': form})
+        if request.method == 'POST':
+            form = ListingForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('homepage')
+
+        else:
+            form = ListingForm()
+        return render(request, 'newlisting.html', {'form': form})
