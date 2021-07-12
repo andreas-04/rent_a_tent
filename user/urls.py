b65cf12d34c86fd214513c3a2e8dcc108bbfb644
Andreas <andreasneacsu@gmail.com>
2021-07-12 14:49:14 -0500

    added temp logout functionality

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.user, name = 'user'),
     path('register', views.register, name = 'register'),
-    path('login', views.user_login, name = 'login' )
+    path('login', views.user_login, name = 'login' ),
+    path('logout', views.logout_view, name='logout'),
 ]
