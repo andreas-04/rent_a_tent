978cc7be36091e15677c95484734458c6ef0f701
Andreas <andreasneacsu@gmail.com>
2021-07-15 15:23:33 -0500

    added name to profile URL

--- user/urls.py
+++ user/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register', views.register, name = 'register'),
     path('login', views.user_login, name = 'login' ),
     path('logout', views.logout_view, name='logout'),
-    path('profile/<slug:username>/', views.getUserProfile)
+    path('profile/<slug:username>/', views.getUserProfile, name = 'getProfile')
 ]
