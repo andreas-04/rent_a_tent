bc7aa0a467e0caa2c889b9cab72f67206e657caf
Andreas <andreasneacsu@gmail.com>
2021-08-01 02:50:42 -0500

    added index view

--- listings/views.py
+++ listings/views.py
@@ -4,7 +4,26 @@ from django.conf import settings
 from home.models import Listing
 from django.views.generic.detail import DetailView
 from django.shortcuts import get_object_or_404
+from django.views.generic.base import TemplateView
 # Create your views here.
+
+
+
+class IndexView(TemplateView):
+
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['listing'] = Listing.objects.all()
+        return context
+
+
+
+
+
+
+
 def listings(request):
     return render(request, 'listings.html')
 
@@ -22,16 +41,13 @@ def new(request):
             form = ListingForm()
         return render(request, 'new.html', {'form': form})
 
-
-
 def getListingAll(request):
     listing = Listing.objects.all
     return render(request, 'all.html', {"listing":listing} )
 
 class listingDetailView(DetailView):
     model = Listing
-    template_name = "listings/listing_detail.html"
-
-
-    def get_object(self, queryset=None):
-        return get_object_or_404(Listing, pk=self.kwargs.get('pk'))
+#    template_name = "listings/listing_detail.html"
+#
+#    def get_object(self, queryset=None):
+#        return get_object_or_404(Listing, pk=self.kwargs.get('pk'))
