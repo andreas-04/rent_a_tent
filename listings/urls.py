a13ad14f00b4b37d8fcf12b83eae0372c72fc085
Andreas <andreasneacsu@gmail.com>
2021-08-10 20:17:14 -0500

    updated urls

--- listings/urls.py
+++ listings/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('new', views.new, name='new_listing'),
     path('all', views.getListingAll, name = 'get_listings_all'),
     path('<slug:slug>/', views.listingDetailView.as_view(), name = "detail-view"),
-#    path('search/<slug:category_slug>/', views.categoryList, name='category_list'),
+#   path('catagory/search/<slug:category_slug>/', views.categoryList, name='category_list'),
+#   path('search/<slug:key_slug>/', views.search, name= "search"),
 ]
 
 
